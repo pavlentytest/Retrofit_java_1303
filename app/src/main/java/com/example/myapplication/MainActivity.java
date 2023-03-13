e5630bc682e6b180f8b3acbc7586c1d604217df5
Pavel <impetus@yandex.ru>
2023-03-13 14:55:45 +0300

    TextChangeListener

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -27,20 +29,36 @@ public class MainActivity extends AppCompatActivity {
     public static final String KEY = "pdct.1.1.20220412T145449Z.ed53b660ddacdc8e.353ee4c0c5adc174b6be636450d97faa6e34a072";
     public static final String LANG = "en";
     public static final Integer LIMIT = 5;
-
+    EditText editText;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.button);
-        btn.setOnClickListener(view -> {
-            doRequest();
+        editText = findViewById(R.id.editTextTextPersonName);
+        textView = findViewById(R.id.result);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editable.toString().length() > 2) {
+                    doRequest();
+                }
+            }
         });
     }
 
     public void doRequest() {
-        EditText editText = findViewById(R.id.editTextTextPersonName);
-        TextView textView = findViewById(R.id.result);
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
